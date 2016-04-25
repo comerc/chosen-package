50241a4f8a21bf8e01359f3c38a3505b9c9b3a93
comerc <comerc@mail.ru>
2016-04-25 15:05:00 +0300

    Update package.js

--- package.js
+++ package.js
@@ -2,15 +2,18 @@ Package.describe({
   name: 'comerc:chosen',
   version: '1.5.1',
   summary: 'Chosen is a library for making long, unwieldy select boxes more friendly.',
-  git: 'https://github.com/eskan/chosen-package',
+  git: 'https://github.com/comerc/chosen-package',
   documentation: 'README.md'
 });
 
 Package.on_use(function (api, where) {
   api.use('jquery@1.4.0');
-
-  api.add_files('chosen.jquery.js', 'client');
-  api.add_files('chosen.css', 'client');
-  api.add_files('chosen-sprite.png', 'client');
-  api.add_files('chosen-sprite@2x.png', 'client');
+  api.addAssets([
+    'chosen-sprite.png',
+    'chosen-sprite@2x.png'
+  ], 'client');
+  api.addFiles([
+    'chosen.jquery.js',
+    'chosen.css'
+  ], 'client');
 });
